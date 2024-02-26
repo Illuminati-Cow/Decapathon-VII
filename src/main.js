c57bea54e9daba4bedb0caaf64d554a3bffb64d5
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-02-26 15:08:38 -0800

    Set up basic config for game and physics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-let config = {
+/**
+ * @type{Phaser.Core.Config}
+ */
+const config = {
     type: Phaser.AUTO,
     width: 640,
     height: 480,
-    scene: []
+    scene: ['Preload', 'Load'],
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true,
+            gravity: {x: 0, y: 0},
+        },
+    },
 }
 
-let game = new Phaser.Game(config)
+const game = new Phaser.Game(config)
