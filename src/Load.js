97829ba0c3a2bc5060e403fcaf931604d0becdd2
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-03-08 23:58:07 -0800

    Add loading bar rough draft animation and polished game opening animation

--- src/Load.js
+++ src/Load.js
@@ -1,53 +1,59 @@
 class Load extends Phaser.Scene {
     constructor() {
         super('Load');
-        this.progressBar;
-        this.progressBox;
-    }
-
-    init () {
-        // Progress Bar
-        this.progressBox = this.add.graphics();
-        this.progressBar = this.add.graphics();
-        let boxConfig = {
-            x: gameSettings.width/2-gameSettings.width/4, 
-            y: gameSettings.height/2-gameSettings.height/10, 
-            width: gameSettings.width/2, 
-            height: gameSettings.height/10
-        }
-        this.progressBox.fillStyle(0x222222, 0.8);
-        this.progressBox.fillRect(boxConfig.x, boxConfig.y, boxConfig.width, boxConfig.height);
     }
 
     /**
      * Loads all assets needed in the game.
      */
     preload() {
-        // Update progress bar based on load status
-        this.load.on('progress', (progress) => {
-            // this.progressBarInternal.setVisible(true)
-            // this.progressBarInternal.scaleX(progress)
-            this.progressBar.clear();
-            this.progressBar.fillStyle(0xffffff, 1);
-            let barConfig = {
-                x: gameSettings.width/2-gameSettings.width/4+5, 
-                y: gameSettings.height/2-gameSettings.height/10+5, 
-                width: gameSettings.width/2-10, 
-                height: gameSettings.height/10-10
-            }
-            this.progressBar.fillRect(barConfig.x, barConfig.y, barConfig.width * progress, barConfig.height);
-        });
         // Load assets
         this.load.path = './assets/'
         // Shapes
         this.load.image('triangle', 'decap_Triangle.png');
-        this.load.image('Squiggle', 'decap_Squiggle.png');
-        this.load.image('Rectangle', 'decap_Rectangle.png');
+        this.load.image('squiggle', 'decap_Squiggle.png');
+        this.load.image('rectangle', 'decap_Rectangle.png');
+        this.load.image('box', 'decap_Box.png');
     }
     /**
      * Creates all global assets needed in the game.
      */
     create() {
-        this.add.image(0,0,'loadscreen').setOrigin(0)
+        let loadscreen = this.add.image(0,0,'loadscreen').setOrigin(0);
+        // Fake progress bar taking one second to load
+        let progressBar = this.add.image(77,255,'progressBar').setOrigin(0).setScale(0,1);
+        let left = this.add.image(0,0,'loadscreenLeft').setOrigin(0).setVisible(false);
+        let right = this.add.image(0,0,'loadscreenRight').setOrigin(0).setVisible(false); 
+        this.tweens.add({
+            targets: progressBar,
+            scaleX: 1,
+            duration: 1000,
+            ease: 'Sine',
+            onComplete: () => {
+                left.setVisible(true);
+                right.setVisible(true);
+                progressBar.setVisible(false);
+                loadscreen.setVisible(false);
+                this.events.emit('tweencomplete');
+                this.scene.launch("MainGame");
+            }
+        });
+        this.events.addListener('tweencomplete', () => {
+            this.tweens.add({
+                targets: left,
+                x: -Settings.width/2 - 25,
+                duration: 1000,
+                ease: 'Cubic'
+            });
+            this.tweens.add({
+                targets: right,
+                x: Settings.width/2,
+                duration: 1000,
+                ease: 'Cubic',
+                onComplete: () => {
+                    this.scene.stop("Load");
+                }
+            });
+        });
     }
 }
